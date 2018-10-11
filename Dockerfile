647b5f5ae285b168235615be6efb076ab59e713b
Henri Pohjalainen <ext-henri.pohjalainen@elisa.fi>
2018-10-11 08:22:28 +0300

    Stuff

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,15 @@
-FROM python:2.7
-LABEL maintainer pohhen
+FROM        selenium/standalone-chrome
+
+USER        root
+# Add user for running tests
+#RUN         usermod -a -G 1000 seluser
+RUN         apt-get update
+#RUN         apt-get install curl -y
+#RUN         curl -sL https://deb.nodesource.com/setup_8.x | bash
+RUN         apt-get install python2.7 python-pip nano -y
+
+#FROM python:2.7
+#LABEL maintainer pohhen
 
 RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
@@ -8,10 +18,17 @@ WORKDIR /usr/src/app
 COPY requirements.txt /usr/src/app/
 RUN pip install --no-cache-dir -r requirements.txt
 
-COPY run.sh /usr/local/bin/run.sh
-RUN chmod +x /usr/local/bin/run.sh
-COPY nop.sh /usr/local/bin/nop.sh
-RUN chmod +x /usr/local/bin/nop.sh
+COPY *.sh /usr/local/bin/
+RUN chmod +x /usr/local/bin/*.sh
+RUN ls -la /usr/local/bin
+#COPY nop.sh /usr/local/bin/nop.sh
+#RUN chmod +x /usr/local/bin/nop.sh
 
+#COPY robot-tests /tmp/tests
+#    environment:
+ENV  HUB_ADDR=hub
+ENV  HUB_PORT=4444
+ENV  ROBOT_TESTS=/tmp/tests
+ENV  OUTPUT_DIR=/output
 # Run in idle process
 CMD run.sh
--- run.sh
+++ run.sh
@@ -1,53 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
-# Inputs
-ROBOT_OPTIONS=$1
-CUSTOM_TEST_DIR=$2
+# Clean output folder
+rm -f ./output/*.html
+rm -f ./output/*.xml
+rm -f ./output/*.png
 
-# Set the defaults
-DEFAULT_LOG_LEVEL="INFO" # Available levels: TRACE, DEBUG, INFO (default), WARN, NONE (no logging)
-DEFAULT_RES="1920x1080x24"
-DEFAULT_DISPLAY=":99"
-DEFAULT_ROBOT_TESTS="false"
-DEFAULT_OUTPUT_DIR="/tmp/"
+echo "start selenium to background"
+/opt/bin/entry_point.sh > /dev/null 2>&1 &
 
-# Use default if none specified as env var
-LOG_LEVEL=${LOG_LEVEL:-$DEFAULT_LOG_LEVEL}
-OUTPUT_DIR=${OUTPUT_DIR:-$DEFAULT_OUTPUT_DIR}
-RES=${RES:-$DEFAULT_RES}
-DISPLAY=${DISPLAY:-$DEFAULT_DISPLAY}
-ROBOT_TESTS=${ROBOT_TESTS:-$DEFAULT_ROBOT_TESTS}
-# echo $HOSTS >> /etc/hosts
+echo "wait for selenium"
+./wait-for-selenium.sh "localhost:4444" "./run_robot.sh"
 
-if [ "$ROBOT_TESTS" = "false" ]; then
-  echo "Error: Please specify the robot test or directory as env var ROBOT_TESTS"
-  exit 1
-elif [ -z "$CUSTOM_TEST_DIR" ]; then
-  echo "Use default test dir"
-  echo "Running tests from: $ROBOT_TESTS"
-elif [ $CUSTOM_TEST_DIR = "all" ]; then
-  echo "Run all tests in every folder"
-  ROBOT_TESTS=/tmp/tests/
-else
-  ROBOT_TESTS=/tmp/tests/$CUSTOM_TEST_DIR
-  echo "Running tests from: $CUSTOM_TEST_DIR"
-fi
+# Kill selenium
+ps uax | grep "selenium\|xvfb" | awk '{print $2}' | xargs kill -9 > /dev/null
 
-if [ ! -z "$ROBOT_OPTIONS" ]; then
-  echo "Executing robot tests with options: $ROBOT_OPTIONS"
-  echo "Use option '-h' to get list of arguments"
-fi
-
-# Clean old screenshots from output folder
-rm  ${OUTPUT_DIR}/*.png 2>/dev/null
-
-# Execute tests
-echo
-echo "#######################################"
-echo "# Running tests                       #"
-echo "#######################################"
-echo
-echo "robot ${ROBOT_OPTIONS} --outputdir ${OUTPUT_DIR} ${ROBOT_TESTS}"
-echo
-
-robot ${ROBOT_OPTIONS} --outputdir ${OUTPUT_DIR} ${ROBOT_TESTS}
+exit 0
